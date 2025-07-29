9b0b3002582ae2ad855d8fd7bc405cc48abe2f83
Shahab Cypher <shahabcypher@gmail.com>
2025-07-29 23:53:59 +0330

    added convert data helper function for charts data

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
+import { convertData } from "../../helpers/convertData";
+
 import styles from "./styles/Chart.module.css";
 
 const Chart = ({ chart, setChart }) => {
+  const [type, setType] = useState("prices");
+
   return (
     <div className={styles.container}>
       <span className={styles.cross} onClick={() => setChart(null)}>
