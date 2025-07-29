f7bd8ccaf04ab2029a5d973cfda974204430d5e7
Shahab Cypher <shahabcypher@gmail.com>
2025-07-29 03:57:50 +0330

    added searched coins list to interface

--- src/components/modules/SearchBox.jsx
+++ src/components/modules/SearchBox.jsx
@@ -7,6 +7,8 @@ const SearchBox = ({ currency, setCurrency }) => {
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setCoins([]);
     if (!search) return;
 
     const searchReq = async () => {
@@ -45,6 +47,16 @@ const SearchBox = ({ currency, setCurrency }) => {
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
+      <div>
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              <img src={coin.thumb} alt={coin.name} />
+              <p>{coin.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
